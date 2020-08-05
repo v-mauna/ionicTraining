85998ac5af7492a8c64f3236a086bde9cae1f2e8
Vanessa Mauna <vmauna@aol.com>
2020-08-05 13:56:14 -0400

    Test files added

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,29 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders without crashing', () => {
+it('renders without crashing', () => {
   const { baseElement } = render(<App />);
   expect(baseElement).toBeDefined();
 });
+
+it('contains three tabs', () => {
+  const { container } = render(<App />);
+  expect(container.querySelectorAll('ion-tab-button').length).toEqual(3);
+});
+
+it.each([
+  [0, 'Current Weather'],
+  [1, 'Forecast'],
+  [2, 'UV Index']
+])('contains the proper text for tab %i', (tab, text) => {
+  const { container } = render(<App />);
+  expect(container.querySelectorAll('ion-tab-button')[tab as number].textContent).toEqual(text);
+});
+
+it('renders consistently', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+
+
